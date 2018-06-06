9a3ae28842500cb01b446537ab4a20c40104edf7
TKK\TKK <1258488863@qq.com>
2018-06-06 16:06:59 +0800

    首次提交

---
+++ app/src/main/java/com/tkksocool/tkk/basketballemperor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tkksocool.tkk.basketballemperor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tkksocool.tkk.basketballemperor.R.layout.activity_main
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+    }
+}
